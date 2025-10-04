9ff425417824f13b7f3ce3c4d0c4b4acbad81cd6
Yuri Miranda Fialho <mirandafialho@gmail.com>
2025-10-04 12:22:38 -0300

    From Design to Blade

---
+++ pixel-position/resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">
+        Laracasts
+    </div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img class="rounded-xl" src="https://placeholdit.com/42x42/dddddd/999999" alt="">
+    </div>
+</div>
